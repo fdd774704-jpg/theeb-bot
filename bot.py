2079a0e4537df457c80253470c413fe5e3a339d0
fdd774704-jpg <fdd774704@gmail.com>
2026-07-14 04:23:54 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,2 @@
-flask import Flask, request
-import telebot
 
-# 1. التوكن الخاص بك تم ترتيبه وتثبيته بشكل صحيح ومستقيم هنا
-BOT_TOKEN = "8611270909:AAHiDyDpMsVAc8IPVXDrVT8-pQUNsl"
-
-# 2. تم تعديل الرابط هنا ليكون رابط تطبيقك الفعلي المخصص على سيرفر Render
-WEBHOOK_URL = "https://theeb-bot-1.onrender.com" 
-
-bot = telebot.TeleBot(BOT_TOKEN)
-app = Flask(__name__)
-
-@ap
 
